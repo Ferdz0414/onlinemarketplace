059e2cff593891f1dec4f5700ed8b2cc86a2bbb9
Ferdenand S. Bayadas <bayadasf04@gmail.com>
2025-06-02 10:36:39 +0800

    Changes for tracking from select to button

--- dashboard.js
+++ dashboard.js
@@ -10,11 +10,4 @@
     });
 
 //PARA SA PAG SELECT NG TRACKING
-const selectoption = document.querySelectorAll('.drop-lis');
-const selected = document.querySelectorAll('.drop-listed');
 
-selected.addEventListener('click', function(e){
-    e.preventDefault();
-
-    const option['.drop-list.value'] = [selectoption];
-});
